593d8cbaee23c9e077c31321f2ffb0b004d7044c
andrewstech <andrew@win11react.com>
2023-04-26 14:54:59 +0000

    GUI Login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,33 @@
 export const LoginPage = () => (
-    <div>
-      <h1>This is the Login Page</h1>
+  <div className="column">
+  <form className="box">
+    <h1 className="title has-text-centered">Login</h1>
+    <div className="field">
+      <label className="label">Email Address</label>
+      <div className="control">
+        <input
+          type="email"
+          placeholder="Enter email"
+          className="input"
+          required
+        />
+      </div>
     </div>
+    <div className="field">
+      <label className="label">Password</label>
+      <div className="control">
+        <input
+          type="password"
+          placeholder="Enter password"
+          className="input"
+          required
+        />
+      </div>
+    </div>
+    <br />
+    <button className="button is-primary" type="submit">
+      Login
+    </button>
+  </form>
+</div>
   );
