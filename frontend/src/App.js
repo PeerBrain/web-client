6c5b0e07729010ca93d5b237763b5f9a3c1e6b88
andrewstech <andrew@win11react.com>
2023-04-26 15:31:37 +0000

    add 404

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "./pages/Login";
 import { HomePage } from "./pages/Home";
 import { RegisterPage } from "./pages/Register";
+import { PageNotFound } from "./pages/PageNotFound";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="*" element={<PageNotFound />} />
 
     </Routes>
   );
---
+++ frontend/src/pages/PageNotFound.js
@@ -0,0 +1,6 @@
+export const PageNotFound = () => (
+    <div class="box">
+        <h1 class="title">404</h1>
+        <p>This is page could not be located.</p>
+    </div>
+    );
