feca29ec641420e8baefadb929b16fdbafe905d3
andrewstech <andrew@win11react.com>
2023-04-28 20:19:13 +0000

    styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,16 @@
 export const HomePage = () => (
-    <div>
-      <p>This is comming soon</p>
+    <div className="column">
+        <div className="box">
+          <h1 className="title has-text-centered">PeerBrain</h1>
+          <div class="columns">
+            <button className="column button is-primary" onclick="window.location.href='https://web.peerbrain.net/login';">
+              Login
+            </button>
+            <button className="column button is-primary" onclick="window.location.href='https://web.peerbrain.net/register';">
+              Register
+            </button>
+          </div>
+          
+        </div>
     </div>
   );
