afe2f99d21b364f2f7abdebd60824ca9798a6614
andrewstech <andrew@win11react.com>
2023-05-23 20:55:34 +0000

    Friends

--- frontend/src/pages/Friends.js
+++ frontend/src/pages/Friends.js
@@ -4,6 +4,15 @@ function Friends () {
     return (
         <div className="box">
             <h1 className='title has-text-centered'>{recipient}</h1>
+                <div className="columns">
+                    <button
+                        className="column button is-primary"
+                        onClick={() => (window.location.href = 'https://web.peerbrain.net/remove-friend/{recipient}')}
+                    >
+                        Remove Friend
+                    </button>
+                    
+                </div>
         </div>
     )
 }
